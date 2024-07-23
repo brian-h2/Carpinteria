455bd9347f5f5e2ec1c9d5c9b7a4087a09e676fd
brian-h2 <brianheredia200309@gmail.com>
2024-07-23 13:21:44 -0300

    config de navbar

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
+import menu from '../../assets/icons/menu.svg'
 import './navbar.css'
 const navbar = () => {
+
+  const menuHambur = useRef()
+  const menuOptions = useRef()
+
+  const mostrarMenu = () => {
+    menuHambur.current.classList.toggle('active');
+    menuOptions.current.classList.toggle('active');
+  }
+
   return (
     <nav className='navbar'>
-        <ul className='navbar-ul'>
+        <ul className='navbar-ul' ref={menuOptions}>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
-        <h2 className='navbar-title'>Title</h2>
+        <h2 className='navbar-title'>Carpinteria</h2>
+        <img ref={menuHambur} onClick={mostrarMenu} className="navbar-icon-burguer"src={menu} alt="nav" srcset="" />
     </nav>
   )
 }
