229757cf916c10476f320509257edde6fede3446
brian-h2 <brianheredia200309@gmail.com>
2024-08-05 20:21:45 -0300

    Mobile First

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './header.css'
+import imagen from '../../assets/background/background-header.jpg';
 
 const header = () => {
   return (
     <header className='header'>
-      <h1>header</h1>
+      <div className='header-background'>
+        <img src={imagen} alt='imagen-header'></img>
+      </div>
+      <div className="header-title">
+        <h1>Aporta tu Idea</h1>
+        <h2>Nosotros la hacemos realidad.</h2>
+      </div>
     </header>
   )
 }
