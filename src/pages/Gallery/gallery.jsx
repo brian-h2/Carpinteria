5bd0f18eab722a2840fe42a5c55cec694334b861
brian-h2 <brianheredia200309@gmail.com>
2024-08-19 10:33:28 -0300

    Fixeando imagenes de la galeria

--- src/pages/Gallery/gallery.jsx
+++ src/pages/Gallery/gallery.jsx
@@ -23,7 +23,7 @@ const gallery = () => {
               <img src={imageGallery2} class="d-block w-100 h-100" alt="imagen-galleria"/>
             </div>
             <div class="carousel-item">
-              <img src={imageGallery1} class="d-block w-30 h-50" alt="imagen-galleria"/>
+              <img src={imageGallery1} class="d-block w-100 h-50" alt="imagen-galleria"/>
             </div>
             <div class="carousel-item">
               <img src={imageGallery3} class="d-block w-100 h-100" alt="imagen-galleria"/>
