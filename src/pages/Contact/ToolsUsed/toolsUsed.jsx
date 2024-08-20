658f80896828234d26170e8fbd988c06e7e22070
brian-h2 <brianheredia200309@gmail.com>
2024-08-19 23:59:34 -0300

    Creacion e importacion de json para mapear imagenes sin repetir tanto codigo

--- src/pages/Contact/ToolsUsed/toolsUsed.jsx
+++ src/pages/Contact/ToolsUsed/toolsUsed.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 import './toolsUsed.css'
+import infoJs from "./images.json"
 
 const toolsUsed = () => {
+
+  const [images,setImages] = useState([])
+
+  useEffect(() => {
+    setImages(infoJs)
+  },[]);
+
+  console.log(images)
+
+
   return (
-    <div>toolsUsed</div>
+    <div>
+      <h1>hola</h1>
+      {images.map((image) => (
+        <div>
+          <h2>{image.id}</h2>
+          <img src={image.direc} alt={image.alt}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
