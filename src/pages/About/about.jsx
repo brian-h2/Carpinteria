ace819562ed5d2c3b6322b016fb3bef93a8a6d91
brian-h2 <brianheredia200309@gmail.com>
2024-08-26 09:52:46 -0300

    Lista la vista de about

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <div className='about'>
         <div className='about-content'>
           <h1>¿Quiénes somos?</h1>
-          <p>Nos dedicamos a la realización de muebles tanto de cocina,vestidores,baños, sala de estar en una amplia gama de colores con el material melamina, altamente resistente y permanente duración</p>
+          <p>Nos dedicamos a la realización de muebles tanto de cocina,vestidores,baños, sala de estar en una amplia gama de colores con el material melamina, altamente resistente y permanente duración.</p>
         </div>
       </div>
     </Fade>
