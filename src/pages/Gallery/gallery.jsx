b3b5e001bc65dade6ccb8ecb5fb154978494a0ad
brian-h2 <brianheredia200309@gmail.com>
2024-08-26 09:45:57 -0300

    Aplicado dimensiones al header img y titulo

--- src/pages/Gallery/gallery.jsx
+++ src/pages/Gallery/gallery.jsx
@@ -16,7 +16,7 @@ const gallery = () => {
       <div className="gallery">
           <h2>Galería</h2>
         <div id="carouselExampleCaptions" class="carousel slide">
-            <div class="carousel-indicators h-15">
+            <div class="carousel-indicators">
               <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
               <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
               <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
